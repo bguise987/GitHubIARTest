4fa229cd6f4844d7556ef387d1bbf7427437fc13
nix7drummer88 <ben@tb-lx-wireless-pittnet-150-212-108-177.wireless.pitt.edu>
2014-05-22 15:48:32 -0400

    Initialize Repo
    Initial Upload of files

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include "io430.h"
+
+int main( void )
+{
+  // Stop watchdog timer to prevent time out reset
+  WDTCTL = WDTPW + WDTHOLD;
+  
+  char message[100] = "Here's some text.";
+  
+  int x = 5;
+  int y = 400;
+
+  return 0;
+}
